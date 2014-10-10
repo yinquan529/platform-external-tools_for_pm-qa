a7bb4bb5b3367a23d2c45d10ce6453e1af9becf1
Vishal Bhoj <vishal.bhoj@hackbox.linaro.org>
2014-10-10 07:54:36 +0100

    Fix build issues for aarch64
    Change-Id: I10735c4cfc7bee430d6693aa978a6d863901ce9d
    Signed-off-by: Vishal Bhoj <vishal.bhoj@hackbox.linaro.org>

--- bc/Android.mk
+++ bc/Android.mk
@@ -5,7 +5,7 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES:= main.c bc.c scan.c execute.c load.c storage.c util.c global.c warranty.c  number.c
 LOCAL_MODULE := bc
 systemtarball: bc
-LOCAL_STATIC_LIBRARIES := libcutils libc
+LOCAL_SHARED_LIBRARIES := libcutils libc
 LOCAL_MODULE_TAGS := tests
 LOCAL_MODULE_PATH := $(TARGET_OUT_EXECUTABLES)
 include $(BUILD_EXECUTABLE)
--- fake_udevadm/Android.mk
+++ fake_udevadm/Android.mk
@@ -5,8 +5,8 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES:= fake_udevadm.c 
 LOCAL_MODULE := fake_udevadm
 systemtarball: fake_udevadm
-LOCAL_STATIC_LIBRARIES := libcutils libc
+LOCAL_SHARED_LIBRARIES := libcutils libc
 LOCAL_MODULE_TAGS := tests
-LOCAL_LDLIBS += -lrt
+#LOCAL_LDLIBS += -lrt
 LOCAL_MODULE_PATH := $(TARGET_OUT_EXECUTABLES)
 include $(BUILD_EXECUTABLE)
